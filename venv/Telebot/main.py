656220558f9fcacddd651bc61e23594c41d786c7
Dmitriy <dimarik_p@ukr.net>
2022-07-16 14:05:37 +0300

    Main

---
+++ venv/Telebot/main.py
@@ -0,0 +1,28 @@
+import MyWeather.getWeather
+import Telebot.Scr.Script
+
+import telebot
+
+bot = telebot.TeleBot('5516859607:AAFyEj5pmMde1oorumRw0t5t47wvf1b-bac')
+
+
+@bot.message_handler(commands=['Start'])
+def Cmd(message):
+   bot.send_message(message.chat.id, "Введите название населенного пункта")
+@bot.message_handler(content_types=['text'])
+def WeatherBot(message):
+    city = message.text
+    print(city)
+    try:
+        ww = MyWeather.getWeather.Weath(city)
+        bot.send_message(message.chat.id, f"Температура в {city}  "
+                                         f"{str(int(ww.temperature('celsius')['temp']))} градусов")
+        bot.send_message(message.chat.id, f"Погода в {city} {Telebot.Scr.Script.Status(ww.status)} ", parse_mode='html')
+    except:
+        bot.send_message(message.chat.id, "Такого города не существует")
+        print("Такого города не существует")
+
+
+print("Start Bot")
+
+bot.polling(none_stop=True)
