4ee18eebd1d48521a940deea5ca239a2da28d5b2
Dmitriy <dimarik_p@ukr.net>
2022-07-16 15:21:03 +0300

    Weather

--- venv/Telebot/MyWeather/getWeather.py
+++ venv/Telebot/MyWeather/getWeather.py
@@ -1,8 +1,10 @@
 from pyowm import OWM
+import Telebot.Token.Token
 
+TokenOWM = Telebot.Token.Token.TokenWeather
 
 def Weath(city):
-    owm = OWM('00eda13e5a9af69a1d143757112a3c28')
+    owm = OWM(TokenOWM)
     mgr = owm.weather_manager()
     observation = mgr.weather_at_place(city)
     w = observation.weather
